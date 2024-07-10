3cb151a97e250042df18ce906310d0ca72e4d863
qinlang <656685187@qq.com>
2024-07-10 14:18:49 +0800

    改成调用属性函数

--- fun_Straight_segment_Mutual_inductance.m
+++ fun_Straight_segment_Mutual_inductance.m
@@ -3,11 +3,12 @@ function M = fun_Straight_segment_Mutual_inductance(l,d)
 %
 % 互感 = fun(线段长度，线段间距)
 
-u0 = 4*pi*1e-7;
+[u0,w] = Attitude('u0','Tape width'); % u0 = 4*pi*1e-7; w带材宽度
+Ld = 2.*sqrt(w/12); % 莱尔定律考虑带材宽度
 
 if nargin == 1 %求解自感
-    M = u0.*l./(2.*pi).*(log(2.*l./5e-6)-0.75); %直线段自感
+    M = u0.*l./(2.*pi).*(log(2.*l./w)-0.75); %直线段自感
 else
-    M = u0.*l./(2.*pi).*(log(2.*l./d)-1); % 两直线段互感
+    M = u0.*l./(2.*pi).*(log(2.*l./sqrt(d.^2 + Ld.^2))-1); % 两直线段互感
 end
 
